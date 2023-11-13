3612d698770e849b4f7309c0fb3f872ffbdd7ed4
ganxxxa <imanzadmajid@gmail.com>
2023-11-13 13:24:18 +0330

    dsdsdfsd

--- app/components/project/Morph.jsx
+++ app/components/project/Morph.jsx
@@ -42,25 +42,21 @@ const Morph = ({
       <div ref={containerRef} className=" w-screen overflow-hidden">
         <section
           ref={sectionRef}
-          className="relative  h-[40vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]"
+          className="relative  h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]"
         >
           <h1 className="text-xl sm:text-4xl mt-12 sm:mt-16 lg:mt-20 px-10 sm:px-24 lg:text-5xl 2xl:text-6xl text-black font-bold w-screen sm:absolute z-40">
             {title}
             <span className={`${titleColor}`}>{title2}</span>
           </h1>
           <div className="absolute top-0  h-full flex items-center   justify-center ">
-            <img
-              className="w-screen h-fit max-h-screen"
-              src={beforeImageSrc}
-              alt="before"
-            />
+            <img className="w-screen  " src={beforeImageSrc} alt="before" />
           </div>
           <div
             ref={imgRef}
             className="absolute translate-x-full h-full flex items-center justify-center overflow-hidden top-0"
           >
             <img
-              className="w-screen h-fit max-h-screen -translate-x-full"
+              className="w-screen   -translate-x-full"
               src={afterImageSrc}
               alt="after"
             />
